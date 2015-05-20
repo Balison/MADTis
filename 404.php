3a6edbc827993d8c0bbc62faebb9b7e5691cd883
Mad Software S.R.L <mad.software.srl@gmail.com>
2015-05-20 09:44:02 -0400

    ahora acepta celulares

--- 404.php
+++ 404.php
@@ -3,7 +3,7 @@
 
  <head>
     <title>SAETIS - Sitio no encontrado</title>
-    <link rel="stylesheet" type="text/css" href="http://localhost/Saetis/css/error404.css">
+    <link rel="stylesheet" type="text/css" href="/css/error404.css">
 </head>
 
 <body>
@@ -11,8 +11,8 @@
 
         <h1 class="page-header"  >Sitio no encontrado</h1>
         <p>Lo sentimos, pero la pagina que busca no existe o no se puede encontrar</p>
-        <a href="http://localhost/Saetis/index.php">Volver a SAETIS</a><br>
-        <img  src="http://localhost/Saetis/imagenes/error404.png">
+        <a href="/index.php">Volver a SAETIS</a><br>
+        <img  src="/imagenes/error404.png">
     </div>
 
 </body>
